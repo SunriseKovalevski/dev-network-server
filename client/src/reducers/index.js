755b7641e7caf8feae44629585f9e57a4b4d4be5
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-01-19 00:25:03 +0300

    feat: add prodile reducer, dashboard

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
+import profileReducer from "./profileReducer";
+import mapReducer from "./mapReducer";
 
 export default combineReducers({
   auth: authReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  profile: profileReducer,
+  map: mapReducer
 });
---
+++ client/src/reducers/mapReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  positionHorizont: 0,
+  positionVertical: 0,
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
